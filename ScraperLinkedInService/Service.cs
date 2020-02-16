200b65d659ae0464fe2081a98b95853a0b02a690
AndrewYehozha <andrew.yehozha@gmail.com>
2020-02-17 01:45:59 +0200

    .

--- ScraperLinkedInService/Service.cs
+++ ScraperLinkedInService/Service.cs
@@ -1,22 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
-using System.Linq;
-using System.ServiceProcess;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ServiceProcess;
 
 namespace ScraperLinkedInService
 {
     public partial class Service : ServiceBase
     {
-        private Scraper scraper;
 
         public Service()
         {
-            scraper = new Scraper();
             InitializeComponent();
         }
 
@@ -31,7 +21,7 @@ namespace ScraperLinkedInService
 
         protected override void OnStop()
         {
-            scraper.Close();
+
         }
 
         public void RunAsConsole(string[] args)
